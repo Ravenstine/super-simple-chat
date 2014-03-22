2148a7372aac7108aed015244e2824383d44b4ad
Ravenstine <benjamin@pixelstreetinc.com>
2014-03-22 16:50:35 -0700

    Fix bad indentation in messages controller.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
 
-	def index
-	end
+  def index
+  end
 
-	def create
-		ActiveSupport::Notifications.instrument('message', params.to_json)
+  def create
+    ActiveSupport::Notifications.instrument('message', params.to_json)
     render nothing: true
-	end
+  end
 
 end
